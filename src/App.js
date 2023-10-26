e772361507ed780421cdd4b95418ce28b4300afe
Pratheesh <pratheesh1218@gmail.com>
2023-10-26 16:33:25 +0530

    rendering the main component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,13 @@
 import "./App.css";
-import Footer from "./components/Footer";
 import Main from "./components/Main";
-import NavBar from "./components/NavBar";
-import UsersList from "./components/UsersList/UsersList";
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import UserDetails from "./components/UserDetails/UserDetails";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AppLayout from "./components/AppLayout";
 
-function App() {
-  const [users, setUsers] = useState([]);
 
-  const loadUsers = async () => {
-    try {
-      const response = await axios.get("https://dummyjson.com/users");
-      setUsers(response.data.users);
-    } catch (error) {
-      console.error("Error fetching users : ", error);
-    }
-  };
+
+const App = () => {
 
   return (
-    // <>
-    //   {/* <NavBar />
-    //   <Main />
-    //   <Footer /> */}
-    //   <NavBar loadUsers={loadUsers} />
-    //   {users.length > 0 && <UsersList users={users} />}
-    // </>
     <>
-      <Router>
-      <AppLayout>
-        <NavBar loadUsers={loadUsers} />
-        <Routes>
-          <Route path="/" element={<UsersList users={users} />} />
-          <Route path="/user/:id" element={<UserDetails />} />
-        </Routes>
-        </AppLayout>
-      </Router>
+      <Main />
     </>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,38 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import UsersList from './UsersList/UsersList'
+import NavBar from "./NavBar";
+import UsersList from "./UsersList/UsersList";
+import React, { useState } from "react";
+import axios from "axios";
+import UserDetails from "./UserDetails/UserDetails";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AppLayout from './AppLayout'
 
 const Main = () => {
-    return (
-        <main>
-            <Routes>
-                <Route path="/" element={<UsersList />}></Route>
-                {/* Add UserDetails routes here... */}
-            </Routes>
-        </main>
-    )
-}
+  const [users, setUsers] = useState([]);
 
-export default Main
+  const loadUsers = async () => {
+    try {
+      const response = await axios.get("https://dummyjson.com/users");
+      setUsers(response.data.users);
+    } catch (error) {
+      console.error("Error fetching users : ", error);
+    }
+  };
 
+  return (
+    <main>
+      <>
+      <Router>
+      <AppLayout>
+        <NavBar loadUsers={loadUsers} />
+        <Routes>
+          <Route path="/" element={<UsersList users={users} />} />
+          <Route path="/user/:id" element={<UserDetails />} />
+        </Routes>
+        </AppLayout>
+      </Router>
+    </>
+    </main>
+  );
+};
+
+export default Main;
